93d023935bae9b2eb366d2619448319ad5be2d69
Dylan Cook <dcook@email.neit.edu>
2023-06-28 17:42:02 -0400

    Still getting this to a working condition. API works but won't properly label route actions.

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RecipiDBlib.Models;
 using RecipiDBlib;
+using Microsoft.EntityFrameworkCore;
 
 namespace Recipi_API.Controllers
 {
@@ -10,69 +11,77 @@ namespace Recipi_API.Controllers
     [ApiController]
     public class PostsController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult GetPostComments(int postId)
+        private readonly RecipiDbContext _context;
+
+        public PostsController(RecipiDbContext context)
         {
-            try
-            {
-                List<PostComment> comments = DatabaseFunctions.GetPostComments(postId);
-                return Ok(comments);
-            } 
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-            
+            _context = context;
         }
-        [HttpPost]
-        public IActionResult PostComment(int postId, string comment)
+
+        [HttpGet("{postId}")]
+        public async Task<ActionResult<List<PostComment>>> GetComments(int postId)
         {
-            try
-            {
-                PostComment postComment = new PostComment();
-                postComment.PostId = postId;
-                postComment.Comment = comment;
-                postComment.CommentDatetime = DateTime.Now;
-                postComment.UserId = -1; //This is a placeholder until we have user routes with proper authentication. We'll need a function to return the HttpContext current user using our own User class.
-                return Ok(postComment);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            //try
+            //{
+            //    List<PostComment> comments = DatabaseFunctions.GetPostComments(postId);
+            //    return Ok(comments);
+            //}
+            //catch (Exception ex)
+            //{
+            //    return BadRequest(ex.Message);
+            //}
+            return await _context.PostComments.Where(p => p.PostId == postId).ToListAsync();
+
         }
-        [HttpPost]
-        public IActionResult LikePost(int postId, int userId) 
-        {
-            try
-            {
-                PostLike pl = new PostLike();
-                pl.PostId = postId;
-                pl.UserId = userId;
-                DatabaseFunctions.LikePost(pl);
-                return Ok();
-            }
-            catch (Exception ex) 
-            {
-                return BadRequest(ex.Message);
-            }
-        }
-        [HttpPost]
-        public IActionResult ReportPost(int postId)
-        {
-            try
-            {
-                PostReport postReport = new PostReport();
-                postReport.PostId = postId;
-                postReport.ReportedDatetime = DateTime.Now;
-                postReport.UserId = -1; //Same placeholder reasoning as above.
-                DatabaseFunctions.AddPostReport(postReport);
-                return Ok();
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
+        //[HttpPost]
+        //public IActionResult PostComment(int postId, string comment)
+        //{
+        //    try
+        //    {
+        //        PostComment postComment = new PostComment();
+        //        postComment.PostId = postId;
+        //        postComment.Comment = comment;
+        //        postComment.CommentDatetime = DateTime.Now;
+        //        postComment.UserId = -1; //This is a placeholder until we have user routes with proper authentication. We'll need a function to return the HttpContext current user using our own User class.
+        //        return Ok(postComment);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
+        //[HttpPost]
+        //public IActionResult PostLike(int postId, int userId) 
+        //{
+        //    try
+        //    {
+        //        PostLike pl = new PostLike();
+        //        pl.PostId = postId;
+        //        pl.UserId = userId;
+        //        DatabaseFunctions.LikePost(pl);
+        //        return Ok();
+        //    }
+        //    catch (Exception ex) 
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
+        //[HttpPost]
+        //public IActionResult PostReportPost(int postId)
+        //{
+        //    try
+        //    {
+        //        PostReport postReport = new PostReport();
+        //        postReport.PostId = postId;
+        //        postReport.ReportedDatetime = DateTime.Now;
+        //        postReport.UserId = -1; //Same placeholder reasoning as above.
+        //        DatabaseFunctions.AddPostReport(postReport);
+        //        return Ok();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using RecipiDBlib.Models;
+using RecipiDBlib;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<RecipiDbContext>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

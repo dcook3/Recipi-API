82bc4165bfce7ddbd509f5a8eb25021047881ffc
Dylan Cook <dcook@email.neit.edu>
2023-07-02 15:19:23 -0400

    Finalized all route code and prepped for rebasing

---
+++ Services/PostInteractionsService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Recipi_API.Models;
+using System.Linq;
+
+
+namespace Recipi_API.Services
+{
+    public class PostInteractionsService : IPostInteractionsService
+    {
+        private readonly IDbContextFactory<RecipiDbContext> _contextFactory;
+        public PostInteractionsService(IDbContextFactory<RecipiDbContext> contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+        public async Task<List<PostComment>> GetComments(int postId)
+        {
+            using(var context = _contextFactory.CreateDbContext())
+            {
+                return await context.PostComments.Where(pc => pc.PostId == postId).ToListAsync();
+            }
+        }
+
+        public async Task<int> PostComment(int postId, string comment)
+        {
+            using (var context = _contextFactory.CreateDbContext())
+            {
+                PostComment pc = new PostComment();
+                pc.PostId = postId;
+                pc.Comment = comment;
+                pc.CommentDatetime = DateTime.Now;
+                context.PostComments.Add(pc);
+                return await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<int> PostLike(int postId, int userId)
+        {
+            using (var context = _contextFactory.CreateDbContext())
+            {
+                PostLike pl = new PostLike();
+                pl.PostId = postId;
+                pl.UserId = userId;
+                context.PostLikes.Add(pl);
+                return await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<int> PostReport(int postId, string message)
+        {
+            using (var context = _contextFactory.CreateDbContext())
+            {
+                PostReport r = new PostReport();
+                r.PostId = postId;
+                r.Message = message;
+                r.ReportedDatetime = DateTime.Now;
+                context.PostReports.Add(r);
+                return await context.SaveChangesAsync();
+            }
+        }
+    }
+}

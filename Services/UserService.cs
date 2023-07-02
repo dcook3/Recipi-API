3390fd5e5f7604df972ce09cd9eda71a9180f4d6
Lucas Johnson <lucas.johnson02@outlook.com>
2023-07-01 21:31:20 -0400

    moving work to pc

---
+++ Services/UserService.cs
@@ -0,0 +1,12 @@
+﻿using RecipiDBlib.Models;
+
+namespace Recipi_API.Services
+{
+    public class UserService
+    {
+        private static RecipiDbContext db = new RecipiDbContext();
+
+        public User? GetUser(int id) => db.Users.Where(user => user.UserId == id).FirstOrDefault();
+        
+    }
+}

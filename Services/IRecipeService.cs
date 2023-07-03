3248cf8bf7a1bb1182775aff2559190669719609
Dylan Cook <dcook@email.neit.edu>
2023-07-03 13:04:39 -0400

    Services created, need to implement into controller

---
+++ Services/IRecipeService.cs
@@ -0,0 +1,13 @@
+﻿using Recipi_API.Models;
+
+namespace Recipi_API.Services
+{
+    public interface IRecipeService
+    {
+        public Task<List<Recipe>> GetRecipeCookbook(int userId, string sortBy);
+        public Task<List<Recipe>> GetRecipeCookbook(int userId);
+        public Task<Recipe> GetRecipeById(int recipeId);
+        public Task<int> CreateRecipe(Recipe recipe);
+        public Task<int> DeleteRecipe(int recipeId);
+    }
+}
---
+++ Services/RecipeService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Recipi_API.Models;
+
+namespace Recipi_API.Services
+{
+    public class RecipeService : IRecipeService
+    {
+        private static RecipiDbContext context = new RecipiDbContext();
+
+        public async Task<int> CreateRecipe(Recipe recipe)
+        {
+            context.Add(recipe);
+            return await context.SaveChangesAsync();
+        }
+
+        public async Task<int> DeleteRecipe(int recipeId)
+        {
+            context.Remove(recipeId);
+            return await context.SaveChangesAsync();
+        }
+
+        public async Task<Recipe> GetRecipeById(int recipeId)
+        {
+            return await context.Recipes.FindAsync(recipeId);
+        }
+
+        public async Task<List<Recipe>> GetRecipeCookbook(int userId, string sortBy)
+        {
+            //Could add more, depending on which sort keys we wish to use.
+            if(sortBy == "author")
+            {
+                return await context.Recipes.Where(r => r.UserId == userId).OrderBy(r => r.User.Username).ToListAsync();
+            }
+            else
+            {
+                return await context.Recipes.Where(r => r.UserId == userId).OrderBy(r => r.CreatedDatetime).ToListAsync();
+            }
+            
+        }
+
+        public async Task<List<Recipe>> GetRecipeCookbook(int userId)
+        {
+            return await context.Recipes.Where(r => r.UserId == userId).ToListAsync();
+        }
+    }
+}

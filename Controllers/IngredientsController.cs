5cbdd4a15809decc70b115c55519f29e09fcdd5b
Dylan Cook <dcook@email.neit.edu>
2023-07-03 13:04:39 -0400

    Services created, need to implement into controller

---
+++ Controllers/IngredientsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Recipi_API.Services;
+
+namespace Recipi_API.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class IngredientsController : ControllerBase
+    {
+        private readonly IIngredientsService _ingredientsService;
+
+        public IngredientsController(IIngredientsService service)
+        {
+            _ingredientsService = service;
+        }
+        //GetIngredientsForRecipe
+        [HttpGet("{recipeId}")]
+        //CreateIngredient
+        [HttpPost("new")]
+    }
+}
---
+++ Services/IIngredientsService.cs
@@ -0,0 +1,10 @@
+﻿using Recipi_API.Models;
+
+namespace Recipi_API.Services
+{
+    public interface IIngredientsService
+    {
+        public Task<List<Ingredient>> GetIngredientsForRecipe(Recipe recipe);
+        public Task<int> CreateIngredient(Ingredient ing);
+    }
+}

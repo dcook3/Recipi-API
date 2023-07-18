48a3070dfe5022b79fb6d1e1c6c424779c3461ac
Dylan Cook <dcook@email.neit.edu>
2023-07-18 10:08:26 -0400

    Moderator routes time. Written and untested

---
+++ Controllers/ModeratorController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Recipi_API.Models;
+using Recipi_API.Services;
+using System.Security.Claims;
+
+namespace Recipi_API.Controllers
+{
+    public class ModeratorController : ControllerBase
+    {
+        private readonly IModeratorService moderatorService;
+        private readonly ClaimsIdentity? _claims;
+
+        public ModeratorController(IModeratorService service, IHttpContextAccessor _context)
+        {
+            moderatorService = service;
+            _claims = (ClaimsIdentity?)_context.HttpContext?.User?.Identity;
+        }
+
+        public async Task<ActionResult> GetPostReports()
+        {
+            try
+            {
+                List<PostReport> prs = await moderatorService.GetPostReports();
+
+                if (prs.Count >= 0)
+                {
+                    return Ok(prs);
+                }
+                return BadRequest("Please check that the post this user has reported exists.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+            
+        }
+
+        public async Task<ActionResult> GetBugReports()
+        {
+            try 
+            { 
+                List<BugReport> brs = await moderatorService.GetBugReports();
+
+                if (brs.Count >= 0)
+                {
+                    return Ok(brs);
+                }
+                return BadRequest("Please check that the post this user has reported exists.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        public async Task<ActionResult> PostReportedPostStatusChange(int reportId, string newStatus)
+        {
+            try
+            {
+                int numrows = await moderatorService.ChangeReportStatus(reportId, newStatus);
+                if (numrows > 0)
+                {
+                    return Ok(numrows);
+                }
+                return BadRequest("Please ensure post report exists, and that the new status is valid.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}

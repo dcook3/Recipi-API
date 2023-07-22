a69599a6dd9393a3fae50ed7e776834f802bf20e
Lucas Johnson <lucas.johnson02@outlook.com>
2023-07-22 13:12:29 -0400

    it was working smh

--- Services/PostFetchService.cs
+++ Services/PostFetchService.cs
@@ -37,13 +37,14 @@ namespace Recipi_API.Services
         public async Task<List<PostPreview>> GetRecommendedPosts(int userId)
         {
             List<PostPreview> postPreviews = new();
-            //increment httpcontext? variable    <- not sure what you mean by this - lj
-            //https://stackoverflow.com/questions/7927329/sql-ordering-records-by-weight could do something like this with dates today bucket, this week bucket, this month bucket
-            List<Post> posts = await context.Posts.Where(p => p.PostInteractions.Select(pi => pi.UserId == userId).Count() == 0)
-                                                  .OrderByDescending(p => p.PostInteractions.GroupBy(pi => pi.PostId)
-                                                                                  .Select(pi => pi.Where(pi => pi.Liked == true).Count()))
-                                                  .ToListAsync();
 
+            //https://stackoverflow.com/questions/7927329/sql-ordering-records-by-weight could do something like this with dates today bucket, this week bucket, this month bucket
+            List<Post> posts = await context.Posts.Where(p => !p.PostInteractions.Any(pi => pi.UserId == userId))
+                                                  .OrderByDescending(p => p.PostInteractions.Where(pi => pi.Liked == true).Count())
+                                                  .ToListAsync();
+            /*
+                                                  .OrderByDescending(p => p.PostInteractions.GroupBy(pi => pi.PostId)
+                                                                                  .Select(pi => pi.Select(pi => pi.Liked == true).Count()))*/
             foreach (Post p in posts)
             {
                 PostPreview postPreview = new();
@@ -54,7 +55,7 @@ namespace Recipi_API.Services
             return postPreviews;
         }
 
-        public async Task<Post> GetSinglePost(int postId) => await context.Posts.FindAsync(postId);
+        public async Task<Post?> GetSinglePost(int postId) => await context.Posts.FindAsync(postId);
 
         public async Task<List<PostPreview>> GetUserPosts(int userId)
         {

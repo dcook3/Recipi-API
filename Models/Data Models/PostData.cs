2c9afb4d0a9c7e439babc5cad30dca705b45e6a1
Lucas Johnson <lucas.johnson02@outlook.com>
2023-07-22 21:46:33 -0400

    temp commit

---
+++ Models/Data Models/PostData.cs
@@ -0,0 +1,15 @@
+﻿namespace Recipi_API.Models.Data_Models
+{
+    public class PostData
+    {
+        public string PostTitle { get; set; } = null!;
+
+        public string PostDescription { get; set; } = null!;
+
+        public int? RecipeId { get; set; }
+
+        public string? PostMedia { get; set; }
+
+        public string ThumbnailUrl { get; set; } = null!;
+    }
+}
